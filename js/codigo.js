9198d943fba66f5132d8b5970a7d9c33f6113d1d
aleramirezsj <aleramirezsj@gmail.com>
2019-10-07 22:49:18 -0300

    menu con jquery, parcial

--- js/codigo.js
+++ js/codigo.js
@@ -3,9 +3,19 @@ $(document).ready(
 	{
         $("#fondo").fadeIn(5000);
         $("#tituloh4").bind("click",mostrarOcultarTitulo);
+        $("#enlacetecnologia").bind("click",mostrarTecnologia);
+        $("#enlacepea").bind("click",mostrarPea);
 	}
 );
 
 function mostrarOcultarTitulo(){
     $("#TituloIndex").fadeToggle();
+}
+function mostrarTecnologia(){
+    $("#planes_de_estudio").fadeOut();
+    $("#tecnologia").fadeIn(2000);
+}
+function mostrarPea(){
+    $("#planes_de_estudio").fadeOut();
+    $("#tecnologia").slideDown(2000);
 }
